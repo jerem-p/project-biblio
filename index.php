1598b794fb516cbbf71552ce7307e956823026f1
Jeremy <jeremy@home>
2020-02-03 13:53:38 +0100

    connected login to db

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php session_start() ?>
+<?php
+session_start();
+$page = 'Accueil';
+?>
+
+
 
 <!DOCTYPE html>
 <html>
@@ -6,12 +11,14 @@
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Project Biblio</title>
+    <title>Project Biblio<?= " - $page" ?></title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <link rel='stylesheet' type='text/css' media='screen' href='css/main.css'>
     <!-- <script src='main.js'></script> -->
 </head>
+
+
 
 <body class="grid">
 
@@ -22,24 +29,31 @@
     <h1 class="top-title">Project Biblio</h1>
     <div class="login-box">
 
+
         <?php
-        if (isset($_SESSION['user'])) {
-            echo $_SESSION['user'] . '<br>' . 
-            $_SESSION['pass'] . '<br>' . 
-            '<a href="php/user_connect.php">Déconnexion</a>';
+        if (isset($_SESSION['user_id'])) {
+        ?>
+
+            Bonjour, <b><?= $_SESSION['user_first_name'] ?></b><br>
+            <a href="php/user_connect.php">Déconnexion</a>
+
+        <?php
         } else {
         ?>
+
             Identifiez-vous :
             <form action="php/user_connect.php" method="get">
-                <input type="text" name="user"><br>
-                <input type="password" name="pass"><br>
+                <input type="text" name="user_id"><br>
                 <button type="submit">Connexion</button><br>
             </form>
+
         <?php
         }
         ?>
 
     </div>
+
+
 
     <nav class="main-navbar">
         <a href="#">Accueil</a>
@@ -47,13 +61,23 @@
         <a href="#">Autre...</a>
     </nav>
 
+
+
     <main class="main-main">
         Main
+        
+
     </main>
+
+
+
 
     <aside class="basket">
         Panier
     </aside>
+
+
+
 
 </body>
 
--- php/user_connect.php
+++ php/user_connect.php
@@ -1,11 +1,23 @@
 <?php
 session_start();
 
-$user = $_GET['user'];
-$pass = $_GET['pass'];
 
-$_SESSION['user'] = $user;
-$_SESSION['pass'] = $pass;
+if (!isset($_GET['user_id'])) {     // depuis le lien "déconnexion"
+    session_destroy();
+} else {
+    $user_id_input = $_GET['user_id'];      // depuis l'inputbox
+
+    $pdo = new PDO('mysql:host=localhost;dbname=biblio_db', 'root', '');
+    $query = $pdo->query(
+        "SELECT * FROM users WHERE user_id='$user_id_input'"    // sql sur db
+    );
+    $user_data = $query->fetch();  // aller chercher les infos de l'utilisateur
+
+    if (isset($user_data['user_id'])) {
+        $_SESSION['user_id'] = $user_data['user_id'];   // confirmation du user_id
+        $_SESSION['user_first_name'] = $user_data['first_name'];    // prénom à afficher en header
+    }
+}
 
 
 header('Location: /index.php');
