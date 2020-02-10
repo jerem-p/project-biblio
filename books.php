b413e087ece5cfef87cac5333f9d32cb4eac3493
Jeremy <jeremy@afpa>
2020-02-10 14:38:41 +0100

    consolidated __dao for booksearch

--- books.php
+++ books.php
@@ -39,16 +39,16 @@ $page = 'Livres';
                 Nom ou prénom: <input type="text" name="author"><wbr>
                 Titre: <input type="text" name="title"><br>
                 Genre: <select name="genre">
+
                     <!-- menu déroulant des thèmes -->
                     <option value='' default>tous</option>
+
                     <?php
-                    $pdo = newPDO();      // function in require '__dao.php';
-                    $query = $pdo->query("SELECT genre_id, genre FROM `genres`");
-                    $data = $query->fetchAll();
-                    foreach ($data as $genre) {
+                    foreach (getBookGenres() as $genre) {   // from __dao.php
                         echo "<option value='$genre[genre_id]'>$genre[genre]</option>";
                     }
                     ?>
+
                 </select>
 
                 <input type="submit" value="Rechercher">
@@ -56,37 +56,12 @@ $page = 'Livres';
             </fieldset>
         </form>
 
-
-
+<!-- ------------------------------------------------------------- -->
 
         <?php
         if ($_SERVER['QUERY_STRING']) {
 
-            $sql = "SELECT DISTINCT genre_id, genre, title, concat(first_name, ' ', last_name) AS author 
-            FROM genres NATURAL JOIN book_genres NATURAL JOIN books NATURAL JOIN book_authors NATURAL JOIN authors";
-            $word = ' WHERE ';          /* premier mot avant les critères de filtrage */
-
-            if ($_GET['author'] != '') {
-                $sql .= $_GET['author'] = $word . "concat(first_name, ' ', last_name) LIKE '%$_GET[author]%'";
-                $word = ' AND ';        /* changement du mot avant les potentiels critères supplémentaires */
-            }
-            if ($_GET['title'] != '') {
-                $sql .= $_GET['author'] = $word . "title LIKE '%$_GET[title]%'";
-                $word = ' AND ';
-            }
-            if ($_GET['genre'] != '') {
-                $sql .= $_GET['author'] = $word . "genre_id = '$_GET[genre]'";
-                $word = ' AND ';
-            }
-
-            $sql .= " GROUP BY title";
-
-            $pdo = newPDO();      // function in require '__dao.php';
-            $query = $pdo->query($sql);
-            $data = $query->fetchAll();
         ?>
-
-
 
             <table id="results">
                 <thead>
@@ -99,7 +74,7 @@ $page = 'Livres';
                 <tbody>
 
                 <?php
-                foreach ($data as $book) {
+                foreach (searchBooks($_GET['author'], $_GET['title'], $_GET['genre']) as $book) {
                     echo "<tr><td>$book[title]</td><td>$book[author]</td><td>$book[genre]</td></tr>";
                 }
             }
@@ -107,9 +82,9 @@ $page = 'Livres';
 
                 </tbody>
             </table>
+<!-- ------------------------------------------------------------- -->
 
-
-            <script>
+            <script>            // chargement de la DATATABLE jquery
                 $(document).ready(function() {
                     $('#results').DataTable();
                 });
